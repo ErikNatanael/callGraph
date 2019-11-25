eb90960b988d4c539f1d76a4a779f08fa0e25361
Erik Natanael Gustafsson <erik@eriknatanael.com>
2019-11-25 15:04:49 +0100

    Up-t-date after exhibition

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,10 @@
 int main( ){
 	ofGLWindowSettings settings;
 	settings.setGLVersion(3,3); // required for OpenGL 3.3 shaders to work
-	// settings.setSize(1920, 1080);
+	settings.setSize(1920, 1080);
 	// settings.setSize(3840, 2160);
 	// settings.setSize(1360, 768);
-	settings.setSize(2560, 1440);
+	// settings.setSize(2560, 1440);
 	settings.windowMode = OF_GAME_MODE;
 	// settings.windowMode = OF_WINDOW;
 	ofCreateWindow(settings);
